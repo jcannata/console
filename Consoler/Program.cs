572214b0a6e92fe926367d3803e511b9b28f2b7c
jcannata <john.cannata@gmail.com>
2014-12-05 08:46:38 -0800

    initial

---
+++ Consoler/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consoler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
