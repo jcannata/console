4222d487f32eda182645fab5ba149752ff499c6f
jcannata <john.cannata@gmail.com>
2014-12-17 09:18:44 -0800

    update

--- src/Consoler/Program.cs
+++ src/Consoler/Program.cs
@@ -1,18 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using System.Threading.Tasks.Dataflow;
+using ReactiveLib;
 
 namespace Consoler
 {
-    class Program
+    internal class Program
     {
+        static async Task<TimeSpan> PerformTransform(CancellationToken token)
+        {
+            DateTime start = DateTime.Now;
+
+            // Create a TransformManyBlock<string, char> object that splits 
+            // a string into its individual characters. 
+            var transformManyBlock = new TransformManyBlock<string, char>(
+                s => s.ToCharArray());
+
+            // Post two messages to the first block.
+            transformManyBlock.Post("Hello");
+            transformManyBlock.Post("World");
+            transformManyBlock.Complete();
+
+            // Receive all output values from the block. 
+            for (int i = 0; i < ("Hello" + "World").Length; i++)
+            {
+                await Task.Delay(TimeSpan.FromMilliseconds(250), token);
+                Console.WriteLine(await transformManyBlock.ReceiveAsync(token));
+            }
+
+            transformManyBlock.Completion.Wait(token);
+
+            return DateTime.Now - start;
+        }
+
         static async Task<TimeSpan> PerformBroadcast(CancellationToken token)
         {
-            var start = DateTime.Now;
+            DateTime start = DateTime.Now;
 
             // Create a BroadcastBlock<double> object. 
             var broadcastBlock = new BroadcastBlock<double>(null);
@@ -23,35 +48,17 @@ namespace Consoler
             // Receive the messages back from the block several times. 
             for (int i = 0; i < 3; i++)
             {
-                var number = await broadcastBlock.ReceiveAsync(token);
+                double number = await broadcastBlock.ReceiveAsync(token);
                 Console.WriteLine(number);
             }
-
-            // Create a TransformManyBlock<string, char> object that splits 
-            // a string into its individual characters. 
-            var transformManyBlock = new TransformManyBlock<string, char>(
-               s => s.ToCharArray());
-
-            // Post two messages to the first block.
-            transformManyBlock.Post("Hello");
-            transformManyBlock.Post("World");
-            transformManyBlock.Complete();
-
-            // Receive all output values from the block. 
-            for (int i = 0; i < ("Hello" + "World").Length ; i++)
-            {
-                await Task.Delay(TimeSpan.FromSeconds(.5), token);
-                Console.WriteLine(await transformManyBlock.ReceiveAsync(token));
-            }
-
-            transformManyBlock.Completion.Wait(token);
-            Console.WriteLine("PerformBroadcast complete");
 
             return DateTime.Now - start;
         }
 
-        static async Task PerformAction()
+        static async Task<TimeSpan> PerformAction(CancellationToken token)
         {
+            DateTime start = DateTime.Now;
+
             // Create an ActionBlock<int> object that prints its input 
             // and throws ArgumentOutOfRangeException if the input 
             // is less than zero. 
@@ -59,17 +66,12 @@ namespace Consoler
             {
                 Console.WriteLine("n = {0}", n);
                 if (n < 0)
-                {
                     throw new ArgumentOutOfRangeException();
-                }
             });
 
             // Create a continuation task that prints the overall  
             // task status to the console when the block finishes.
-            throwIfNegative.Completion.ContinueWith(task =>
-            {
-                Console.WriteLine("The status of the completion task is '{0}'.", task.Status);
-            });
+            var finalFinal = throwIfNegative.Completion.ContinueWith(task => Console.WriteLine("The status of the completion task is '{0}'.", task.Status), token);
 
             // Post values to the block.
             throwIfNegative.Post(0);
@@ -81,7 +83,8 @@ namespace Consoler
             // Wait for completion in a try/catch block. 
             try
             {
-                throwIfNegative.Completion.Wait();
+                throwIfNegative.Completion.Wait(token);
+                finalFinal.Wait(token);
             }
             catch (AggregateException ae)
             {
@@ -90,20 +93,66 @@ namespace Consoler
                 ae.Handle(e =>
                 {
                     Console.WriteLine("Encountered {0}: {1}",
-                       e.GetType().Name, e.Message);
+                        e.GetType().Name, e.Message);
                     return true;
                 });
             }
+
+            return DateTime.Now - start;
+        }
+
+        static async Task<TimeSpan> PerformBatch(CancellationToken token)
+        {
+            var start = DateTime.Now;
+
+            // Create a BatchBlock<int> object that holds ten 
+            // elements per batch. 
+            var batchBlock = new BatchBlock<int>(10);
+
+            var doSomething = new ActionBlock<int[]>(a =>
+            {
+                Console.WriteLine("The sum of the elements is {0}.", a.Sum());
+            });
+
+            batchBlock.LinkTo(doSomething);
+            var finalFinal = batchBlock.Completion.ContinueWith(delegate { doSomething.Complete(); }, token);
+
+            // Post several values to the block. 
+            for (var i = 0; i < 13; i++)
+            {
+                batchBlock.Post(i);
+            }
+
+            // Set the block to the completed state. This causes 
+            // the block to propagate out any any remaining 
+            // values as a final batch.
+            batchBlock.Complete();
+            //await finalFinal;
+            doSomething.Completion.Wait(token);
+
+            // Print the sum of both batches.
+
+            return DateTime.Now - start;
         }
 
         static async Task MainAsync(string[] args, CancellationToken token)
         {
             try
             {
+                var reactiveSample = new Class1(token);
+                reactiveSample.Time();
+                return;
+
+                // start tasks...
+                var batch = PerformBatch(token);
                 var broadcast = PerformBroadcast(token);
-                await PerformAction();
-                var time = await broadcast;
-                Console.WriteLine(time);
+                var action = PerformAction(token);
+                var transform = PerformTransform(token);
+
+                Console.WriteLine("broadcast:{0}", await broadcast);
+                Console.WriteLine("action:{0}", await action);
+                Console.WriteLine("transform:{0}", await transform);
+                Console.WriteLine("batch:{0}", await batch);
             }
             catch (Exception e)
             {
@@ -114,15 +163,16 @@ namespace Consoler
 
         static void Main(string[] args)
         {
-            CancellationTokenSource cts = new CancellationTokenSource();
+            var cts = new CancellationTokenSource();
 
-            System.Console.CancelKeyPress += (s, e) =>
+            Console.CancelKeyPress += (s, e) =>
             {
                 e.Cancel = true;
                 cts.Cancel();
             };
 
             MainAsync(args, cts.Token).Wait();
+            Console.WriteLine("Press any key to exit");
             Console.ReadLine();
         }
     }
---
+++ src/ReactiveLib/Class1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReactiveLib
+{
+    public class Class1
+    {
+        readonly CancellationToken _token;
+
+        public Class1(CancellationToken token)
+        {
+            _token = token;
+        }
+
+        public void Time()
+        {
+            Console.WriteLine(DateTime.Now);
+
+            // create a single event in 10 seconds time
+            var observable = Observable.Timer(TimeSpan.FromSeconds(10)).Timestamp();
+
+            // raise exception if no event received within 9 seconds
+            var observableWithTimeout = observable.Timeout(TimeSpan.FromSeconds(9));
+
+            using (observableWithTimeout.Subscribe(
+                x => Console.WriteLine("{0}: {1}", x.Value, x.Timestamp),
+                ex => Console.WriteLine("{0} {1}", ex.Message, DateTime.Now)))
+            {
+                Console.WriteLine("Press any key to unsubscribe");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+
+        public void Test()
+        {
+            var observable = Observable.Interval(TimeSpan.FromMilliseconds(750)).TimeInterval();
+
+            using (observable.Subscribe(
+                x => Console.WriteLine("{0}: {1}", x.Value, x.Interval)))
+            {
+                Console.WriteLine("Press any key to unsubscribe");
+                Console.ReadKey();
+            }
+
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
+        }
+    }
+}
